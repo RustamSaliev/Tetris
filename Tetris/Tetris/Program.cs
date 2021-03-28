a8e440192f87f372b1f351f8d1ae951312ccf41d
Rustam <rustam0295@list.ru>
2021-03-28 20:25:41 +0500

    Drow => Draw

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -15,8 +15,8 @@ namespace Tetris
 
             Drow(x1, y1, c1);
 
-            int x2 = 4;
-            int y2 = 6;
+            int x2 = 2;
+            int y2 = 3;
             char c2 = '#';
 
             Drow(x2, y2, c2);
